ddfacd5b7b06b199600670f49f2f2577326da902
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-16 15:33:11 -0700

    completed selection sort, did not use what was set up for me except the len(arr) - 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,13 +1,36 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+
+    # for i in range(0, len(arr) - 1):
+    #     cur_index = i
+    #     smallest_index = cur_index
+
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+    i = 0   
+    for i in range(0, len(arr)-1): #doesn't include the very last one because it should already be sorted to proper position, would break if tried?
 
+    #a. Loop through elements on right-hand-side 
+    #of current index and find the smallest element
+            #assume current number is smallest, 
+           
+            j = i+1
+            while j in range(i+1, len(arr)): #DOES include the very last one because we need to check it's value against everything that comes before it, not including it here would make it so that we didn't compare 8 to 7 and it would think it was done with 8,9, (invisible) 7
+                smallest = arr[i]
+
+                if smallest <= arr[j]: #smallest less than the next_number
+                    j +=1 
+                else:
+
+                    smallest = arr[j]
+                    arr[j] = arr[i]  
+                    arr[i] = smallest
+                     
+                    
+    #b. Swap the element at current index with the
+    #smallest element found in above loop
+  
 
 
         # TO-DO: swap
