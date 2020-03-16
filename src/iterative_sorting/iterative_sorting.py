d4144468d3a2f4b763ee7c9c3510c618746fbc38
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-16 14:32:38 -0700

    bubble sort completed? not showing as Failure and print outs showing arrays sorted

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,6 +20,25 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    i = 0
+    while i in range(0, len(arr) - 1): #arr[i+1] and 
+
+        # if i > (len(arr) - 1):
+        #     return
+        arr = arr
+        if arr[i] > arr[i+1]:
+            smaller = arr[i+1]
+            larger = arr[i]
+
+            arr[i] = smaller
+            arr[i+1] = larger
+            i = 0
+
+        elif arr[i] < arr[i+1]:
+
+            i += 1
+
+
 
     return arr
 
